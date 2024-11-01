71fed58c11d2407532273816a25e5b04e4f57e27
shahzad2003 <shahzad.2023012005@tulas.edu.in>
2024-11-02 01:13:50 +0530

    Updates

---
+++ .history/src/App_20241102003144.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route, Switch } from "react-router-dom";
+// Components
+import MyNavbar from "./components/Nav";
+import Categories from "./components/Categories";
+import Questions from "./components/Question";
+
+function App() {
+  return (
+    <>
+      <MyNavbar />
+      <Routes>
+        <Route path="/" element={<Categories />} />{" "}
+        {/* Render Categories on Home */}
+        <Route path="*" element={<Categories />} />
+        {/* Dynamic category route */}
+        <Route path="/:category" element={<Questions />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
